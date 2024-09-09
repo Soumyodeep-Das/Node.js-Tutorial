ff7c3e0d0bac3f962371b53b8c4c3f64f5ba3548
Soumyodeep-Das <linux.programmer.06@gmail.com>
2024-09-09 23:37:52 +0530

    Tenth Commit

--- 04_REST_API/index.js
+++ 04_REST_API/index.js
@@ -1,9 +1,14 @@
 const express = require("express")
 const users = require('./MOCK_DATABASE.json')
+const fs = require("fs")
 
 const app = express()
 
 const PORT = 8000
+
+
+// Middleware | Plugin
+app.use(express.urlencoded({extended: false}))
 
 // Routes
 
@@ -22,10 +27,53 @@ app.get("/users", (req, res) => {
 })
 
 // Dynamic Route to get specific user data (By id)
-app.get("/api/users/:id", (req, res) => {
-    const id = Number(req.params.id)
-    const user = users.find((user) => user.id === id)
-    return res.json(user)
-})
+// app.get("/api/users/:id", (req, res) => {
+//     const id = Number(req.params.id)
+//     const user = users.find((user) => user.id === id)
+//     return res.json(user)
+// })
 
+app
+    .route("/api/users/:id")
+    .get((req, res) => {
+        const id = Number(req.params.id)
+        const user = users.find((user) => user.id === id)
+        return res.json(user)
+    })
+    .put((req, res) => {
+        const id = Number(req.params.id);
+        const body = req.body;
+        const index = users.findIndex((user) => user.id === id);
+        if (index !== -1) {
+            users.splice(index, 1);
+            users.push({id: id, ...body});
+            fs.writeFile('./MOCK_DATABASE.json', JSON.stringify(users), () => {
+                return res.json({status: 'update success', id: id});
+            });
+        } else {
+            return res.status(404).json({status: 'user not found'});
+        }
+    })
+    .delete((req, res) => {
+        const id = Number(req.params.id);
+        const index = users.findIndex((user) => user.id === id);
+        if (index !== -1) {
+            users.splice(index, 1);
+            fs.writeFile('./MOCK_DATABASE.json', JSON.stringify(users), () => {
+                return res.json({status: 'delete success', id: id});
+            });
+        } else {
+            return res.status(404).json({status: 'user not found'});
+        }
+    })
+
+app
+.post("/api/users", (req, res) => {
+    const body = req.body;
+    // console.log(body)
+    users.push({id: users.length+1, ...body})
+    fs.writeFile('./MOCK_DATABASE.json', JSON.stringify(users), (err, data) => {
+        return res.json({status: "success", id: users.length})
+    })
+})
 app.listen(PORT, () => console.log(`Server has started at ${PORT}.`))
